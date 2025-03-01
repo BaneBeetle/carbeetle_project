5b86e6749ae4641a6bb9abd59da00e5d74a4f342
Brian <banebeetle@gmail.com>
2025-03-01 01:11:04 -0800

    Expanded carbeetle dataset + rename function

--- scaler.py
+++ scaler.py
@@ -23,7 +23,25 @@ def downscale(input_folder, output_folder):
             except Exception as e:
                 print(f"ERROR ON {filename}: {e}")
 
+def rename_files(input_folder, base_name):
+    items = os.listdir(input_folder)
+    files = [f for f in items if os.path.isfile(os.path.join(input_folder, f))]
+    files.sort()
+
+    for counter, filename in enumerate(files, start=1):
+        _, ext = os.path.splitext(filename)
+        new_filename = f"{base_name}{counter}{ext}"
+        old_path = os.path.join(input_folder, filename)
+        new_path = os.path.join(input_folder, new_filename)
+        os.rename(old_path, new_path)
+
+
 if __name__ == '__main__':
-    input_folder = "carbeetle" # INSERT YO FOLDER HERE
-    output_folder = "64_carbeetle" # PUT YO OUTPUT HERE
-    downscale(input_folder, output_folder)
+
+    #input_folder = "carbeetle" # INSERT YO FOLDER HERE
+    #output_folder = "64_carbeetle" # PUT YO OUTPUT HERE
+    #downscale(input_folder, output_folder)
+
+    # rename_files("64_carbeetle", "carbeetle_") # I messed up the naming convention 
+    pass
+
