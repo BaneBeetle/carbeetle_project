e6a1c61c1b8c49c1cf7cb408f956dbc2d54e99f0
Brian <banebeetle@gmail.com>
2025-03-04 10:20:50 -0800

    Removed old dataset, added roboflow dataset, fine-tuned YOLO v11 and got model in /train3/best.pt

---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# Detect And Classify Object Detection > 2025-03-03 10:26pm
+https://universe.roboflow.com/carbeetle/detect-and-classify-object-detection-woezq
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- training.py
+++ training.py
@@ -1,3 +1,21 @@
 # Brian Phan
 # 84609992
 
+from ultralytics import YOLO
+
+def main():
+    model = YOLO("yolo11m.pt")
+    model.train(
+        data="C:\\Users\\lolly\\OneDrive\\Desktop\\Projects\\carbeetle_project\\data.yaml",  # path to yaml file  
+        imgsz=640,  # image size for training  
+        batch=8,   # batch size  
+        epochs=100,  # number of epochs  
+        device=0   # use GPU if available, else CPU
+    )
+
+def main1():
+    model = YOLO("C:\\Users\\lolly\\OneDrive\\Desktop\\Projects\\carbeetle_project\\runs\detect\\train3\\weights\\best.pt")
+    model.predict(source="test//images//carbeetle_00218259_jpg.rf.67814291d0af6e198b432712ba88cc82.jpg", show = True, save = True, line_width = 2)
+if __name__ == "__main__":
+    main1()
+
